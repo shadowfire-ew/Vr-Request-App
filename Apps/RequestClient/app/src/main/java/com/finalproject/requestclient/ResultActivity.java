376c207e47c33805c0729c7a6797f8ab86bf24a0
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-06-15 14:28:15 -0400

    catching the status from the notification intent

--- Apps/RequestClient/app/src/main/java/com/finalproject/requestclient/ResultActivity.java
+++ Apps/RequestClient/app/src/main/java/com/finalproject/requestclient/ResultActivity.java
@@ -2,13 +2,25 @@ package com.finalproject.requestclient;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        tv = findViewById(R.id.result);
+
+        Intent i = getIntent();
+
+        String result = i.getStringExtra(RequestChangeChecker.TAG);
+
+        tv.setText("Your status is now: "+result);
     }
 }
