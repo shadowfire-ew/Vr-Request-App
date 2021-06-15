7a5978c759f40fbf4a07d2a7d580f9d722c693be
shadowfire-ew <55920006+shadowfire-ew@users.noreply.github.com>
2021-06-15 14:14:41 -0400

    completely reworked the notification
    now uses:
    - notification compat and notification manager compat
    - an intent to go to when clicked
    Also reworked layout for readability

--- Apps/RequestClient/app/src/main/java/com/finalproject/requestclient/RequestChangeChecker.java
+++ Apps/RequestClient/app/src/main/java/com/finalproject/requestclient/RequestChangeChecker.java
@@ -2,11 +2,16 @@ package com.finalproject.requestclient;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.work.WorkManager;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
@@ -19,6 +24,7 @@ import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class RequestChangeChecker extends Worker {
+    public static String TAG = "status_change_result";
     private boolean waiting = true;
     private boolean open = true;
     private String statusChanged;
@@ -62,12 +68,37 @@ public class RequestChangeChecker extends Worker {
             // do nothing
         }
         if(!open){
+            Log.d("not open","starting process");
+            // update the local status
             SharedPreferences.Editor edit = preferences.edit();
-            edit.putBoolean(MainActivity.REQUEST_KEY,false);
-            NotificationManager NM = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-            Notification notify = new Notification.Builder(getApplicationContext(),MainActivity.CHANNEL_ID).setContentText("Status changed to "+statusChanged)
-                    .setContentTitle("Request status changed!").setSmallIcon(R.drawable.ic_launcher_background).build();
+            edit.putBoolean(MainActivity.REQUEST_KEY,false).apply();
+
+            // make the notification manager
+            NotificationManagerCompat NM = NotificationManagerCompat.from(context);
+
+            // make the notification builder
+            NotificationCompat.Builder notifyBuilder = new NotificationCompat.Builder(getApplicationContext(),MainActivity.CHANNEL_ID);
+
+            // creating an intent to go to main page when approved
+            Intent intent = new Intent(context,MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK|Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            intent.putExtra(TAG,statusChanged);
+            PendingIntent pIntent = PendingIntent.getActivity(context,0,intent,0);
+
+            // build the notification
+            Notification notify = notifyBuilder
+                    .setContentText("Status changed to "+statusChanged)
+                    .setContentTitle("Request status changed!")
+                    .setSmallIcon(R.drawable.ic_launcher_foreground)
+                    .setPriority(NotificationCompat.PRIORITY_DEFAULT) // for backwards compat
+                    .setContentIntent(pIntent)
+                    .setAutoCancel(true)
+                    .build();
+
+            // displaying the notification
             NM.notify(0,notify);
+
+            //canceling the work
             WorkManager.getInstance(context).cancelWorkById(this.getId());
         }
         return Result.success();
